dbb7fcb103475aa0cc4ce37d6c11a788a01b9687
Jaimin Bhut <jaimin.bhut@bacancy.com>
2024-02-27 14:47:33 +0530

    feat: initialize project

---
+++ App.tsx
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+  useColorScheme,
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    flex: 1,
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View>
+        <Text style={{ color: !isDarkMode ? Colors.darker : Colors.lighter }}>
+          Supabase Demo
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
